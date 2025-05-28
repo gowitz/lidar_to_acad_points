94944661058c6363927fdf37c8ad72a5c714e4aa
Fab <fab@fab-lab-local>
2025-05-29 00:04:13 +0200

    Fix check extension et integration fichier json classifications V2.0.1

--- app.py
+++ app.py
@@ -9,13 +9,24 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy
 import subprocess
+import json
 
+APP_VERSION = "2.0.1 beta"
 UPLOAD_FOLDER = 'uploads'
 DOWNLOAD_FOLDER = 'downloads'
 EXPORT_FILENAME = 'points.scr'
-ALLOWED_EXTENSIONS = ['csv','txt','dxf']
-LIDAR_CLASSIFICATION_IGNORE = [4,5,6]
+ALLOWED_EXTENSIONS = {'profil': 'csv','lidar': 'csv','dxf2kml': 'dxf', 'vc_lidar':'csv'}
+#LIDAR_CLASSIFICATION_IGNORE = [4,5,6]
+LIDAR_CLASSIFICATION_IGNORE = []
+CLASSIFICATION_FILE = "static/classification_lidar_cloud_points.json"
 MNT_ONLY_DEFAULT = False
+
+with open(CLASSIFICATION_FILE, "r") as f:
+    data = json.load(f)
+for item in data:
+    if not item['mnt']:
+        LIDAR_CLASSIFICATION_IGNORE.append(int(item['classification_code']))
+f.close()
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -26,39 +37,42 @@ app.secret_key = "secret key"  # for encrypting the session
 
 # Upload en check file section ------------------------------------------------ 
 # Check allowed file extension
-def allowed_file_extension(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+def allowed_file_extension(filename:str, conversion: str) -> bool:
+    extension = get_file_extension(filename)
+    return True if extension == ALLOWED_EXTENSIONS[conversion] else False
 
 def get_file_extension(filename: str) -> str:
     return filename.rsplit('.', 1)[1].lower()
 
-def upload_file(request):
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part', 'danger')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file', 'danger')
-            return redirect(request.url)
-        if not allowed_file_extension(file.filename):
-            flash('Wrong file type, only .csv file or .dxf file', 'danger')
-            return redirect(request.url)
-        filename = secure_filename(file.filename)
-        # if file already exists in upload folder remove it
-        if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
-            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        # then save the file
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return filename
+def check_file_to_upload(request, conversion):
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part', 'danger')
+        return False
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit an empty part without filename
+    if file.filename == '':
+        flash('No selected file', 'danger')
+        return False
+    if not allowed_file_extension(file.filename, conversion):
+        flash('Wrong file type, only .' + ALLOWED_EXTENSIONS[conversion] + ' file', 'danger')
+        return False
+    return True
+
+def upload_file(request: Flask) -> str:
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    # if file already exists in upload folder remove it
+    if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], filename)):
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    # then save the file
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return filename
 
 # Output file section ---------------------------------------------------------   
 # Set output file
-def set_script_file(filename):
+def set_script_file(filename: str) -> str:
     # if default file already exists in destination folder remove it
     if os.path.isfile(os.path.join(app.config['DOWNLOAD_FOLDER'], app.config['EXPORT_FILENAME'])):
         os.remove(os.path.join(
@@ -137,6 +151,7 @@ def plot_lines(export_filename, mnt):
 
     return plt
 
+# Plot points
 def plot_points(export_filename, mnt):
     filename = export_filename.split(".")[0] + ".csv"
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
@@ -167,6 +182,8 @@ def plot_points(export_filename, mnt):
     plt.scatter(x,y, c='k', s=0.1)
 
     return plt
+
+
 
 # Routes ----------------------------------------------------------------------
 # home 
@@ -225,37 +242,38 @@ def lidar():
     return render_template('upload_lidar.html')
 
 @ app.route('/lidar', methods=['POST'])
-def upload_lidar():
+def upload_lidar() -> None:
     if request.method == 'POST':
-        filename = upload_file(request)
-        export_filename = set_script_file(filename)
-        mnt_checkbox = request.form.get('mnt')
+        if check_file_to_upload(request, 'lidar') :
+            filename = upload_file(request)
+            export_filename = set_script_file(filename)
+            mnt_checkbox = request.form.get('mnt')
 
-        if mnt_checkbox == 'mnt':
-            mnt = True
-        else:
-            mnt = MNT_ONLY_DEFAULT
+            if mnt_checkbox == 'mnt':
+                mnt = True
+            else:
+                mnt = MNT_ONLY_DEFAULT
 
-         # create dataframe
-        df = pd.read_csv(os.path.join(
-            app.config['UPLOAD_FOLDER'], filename))
-        # if columns distance, altitude and classification are in dataframe
-        if "distance" in df and "altitude" in df and "classification" in df:
-            # skip MNS classifications
-            if mnt:
-                df = df[~df.classification.isin(LIDAR_CLASSIFICATION_IGNORE)]
-            # filtre dataframe columns
-            df = df[['distance', 'altitude', 'classification']]
-            df.rename(columns={'distance':'x', 'altitude':'y'}, inplace=True)
-            # group by classification
-            gb = df.groupby('classification')
-            write_points(export_filename, gb)
+            # create dataframe
+            df = pd.read_csv(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename))
+            # if columns distance, altitude and classification are in dataframe
+            if "distance" in df and "altitude" in df and "classification" in df:
+                # skip MNS classifications
+                if mnt:
+                    df = df[~df.classification.isin(LIDAR_CLASSIFICATION_IGNORE)]
+                # filtre dataframe columns
+                df = df[['distance', 'altitude', 'classification']]
+                df.rename(columns={'distance':'x', 'altitude':'y'}, inplace=True)
+                # group by classification
+                gb = df.groupby('classification')
+                write_points(export_filename, gb)
 
-        else:
-            flash('File not well formatted', 'danger')
-            return redirect(request.url)
-        #return redirect('/downloadlidarfile/' + export_filename + mnt)
-        return redirect(url_for('download_lidar_file', export_filename=export_filename, mnt = str(mnt)))
+            else:
+                flash('File not well formatted', 'danger')
+                return redirect(request.url)
+            #return redirect('/downloadlidarfile/' + export_filename + mnt)
+            return redirect(url_for('download_lidar_file', export_filename=export_filename, mnt = str(mnt)))
 
     return render_template('upload_lidar.html')
 
@@ -266,30 +284,31 @@ def profil():
 @ app.route('/profil', methods=['POST'])
 def upload_profil():
     if request.method == 'POST':
-        filename = upload_file(request)
-        export_filename = set_script_file(filename)
-        mnt_checkbox = request.form.get('mnt')
+        if check_file_to_upload(request, 'profil') :
+            filename = upload_file(request)
+            export_filename = set_script_file(filename)
+            mnt_checkbox = request.form.get('mnt')
 
-        if mnt_checkbox == 'mnt':
-            mnt = True
-        else:
-            mnt = MNT_ONLY_DEFAULT
+            if mnt_checkbox == 'mnt':
+                mnt = True
+            else:
+                mnt = MNT_ONLY_DEFAULT
 
-         # create dataframe
-        df = pd.read_csv(os.path.join(
-            app.config['UPLOAD_FOLDER'], filename))
-        # if columns distance, altitude and classification are in dataframe
-        if "distance" in df and "mnt" in df and "mns" in df:
-            write_lines(export_filename, df, mnt)
+            # create dataframe
+            df = pd.read_csv(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename))
+            # if columns distance, altitude and classification are in dataframe
+            if "distance" in df and "mnt" in df and "mns" in df:
+                write_lines(export_filename, df, mnt)
 
-        else:
-            flash('File not well formatted', 'danger')
-            return redirect(request.url)
+            else:
+                flash('File not well formatted', 'danger')
+                return redirect(request.url)
 
-        return redirect(url_for('download_profile_file', export_filename=export_filename, mnt = str(mnt)))
+            return redirect(url_for('download_profile_file', export_filename=export_filename, mnt = str(mnt)))
         
 
-    return render_template('upload_lidar.html')
+    return render_template('upload_profil.html')
 
 @ app.route('/dxf2kml')
 def dxf2kml():
@@ -298,17 +317,15 @@ def dxf2kml():
 @ app.route('/dxf2kml', methods=['POST'])
 def upload_dxf2kml():
     if request.method == 'POST':
-        filename = upload_file(request)
-        print(filename)
-        if get_file_extension(filename) == 'dxf':
-            export_filename = filename.split(".")[0] + ".kml"
-            input_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            output_file = os.path.join(app.config['DOWNLOAD_FOLDER'], export_filename)
-            print(output_file)
-            print(input_file)
-            cmd = 'ogr2ogr -f KML -s_srs epsg:2056 -t_srs epsg:4326 ' + output_file + ' ' + input_file
-            subprocess.call(cmd, shell=True)
-        return redirect(url_for('download_dxf2kml_file', export_filename=export_filename))
+        if check_file_to_upload(request, 'dxf2kml') :
+            filename = upload_file(request)
+            if get_file_extension(filename) == 'dxf':
+                export_filename = filename.split(".")[0] + ".kml"
+                input_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                output_file = os.path.join(app.config['DOWNLOAD_FOLDER'], export_filename)
+                cmd = 'ogr2ogr -f KML -s_srs epsg:2056 -t_srs epsg:4326 ' + output_file + ' ' + input_file
+                subprocess.call(cmd, shell=True)
+            return redirect(url_for('download_dxf2kml_file', export_filename=export_filename))
     return render_template('upload_dxf2kml.html')
 
 @ app.route('/vc_lidar')
@@ -318,36 +335,37 @@ def vc_lidar():
 @ app.route('/vc_lidar', methods=['POST'])
 def upload_vc_lidar():
     if request.method == 'POST':
-        filename = upload_file(request)
-        export_filename = set_script_file(filename)
-        mnt_checkbox = request.form.get('mnt')
+        if check_file_to_upload(request, 'vc_lidar') :
+            filename = upload_file(request)
+            export_filename = set_script_file(filename)
+            mnt_checkbox = request.form.get('mnt')
 
-        if mnt_checkbox == 'mnt':
-            mnt = True
-        else:
-            mnt = MNT_ONLY_DEFAULT
-         # create dataframe
-        df = pd.read_csv(os.path.join(
-            app.config['UPLOAD_FOLDER'], filename))
-        df.columns = df.columns.str.strip()
-        # if columns distance, altitude and classification are in dataframe
-        if "mileage" in df and "z" in df and "classification" in df:
-            # skip MNS classifications
-            if mnt:
-                df = df[~df.classification.isin(LIDAR_CLASSIFICATION_IGNORE)]
-            # filtre dataframe columns
-            df = df[['mileage', 'z', 'classification']]
-            df.rename(columns={'mileage':'x', 'z':'y'}, inplace=True)
+            if mnt_checkbox == 'mnt':
+                mnt = True
+            else:
+                mnt = MNT_ONLY_DEFAULT
+            # create dataframe
+            df = pd.read_csv(os.path.join(
+                app.config['UPLOAD_FOLDER'], filename))
+            df.columns = df.columns.str.strip()
+            # if columns distance, altitude and classification are in dataframe
+            if "mileage" in df and "z" in df and "classification" in df:
+                # skip MNS classifications
+                if mnt:
+                    df = df[~df.classification.isin(LIDAR_CLASSIFICATION_IGNORE)]
+                # filtre dataframe columns
+                df = df[['mileage', 'z', 'classification']]
+                df.rename(columns={'mileage':'x', 'z':'y'}, inplace=True)
 
-            # group by classification
-            gb = df.groupby('classification')
-            write_points(export_filename, gb)
+                # group by classification
+                gb = df.groupby('classification')
+                write_points(export_filename, gb)
 
-        else:
-            flash('File not well formatted', 'danger')
-            return redirect(request.url)
+            else:
+                flash('File not well formatted', 'danger')
+                return redirect(request.url)
 
-        return redirect(url_for('download_lidar_file', export_filename=export_filename, mnt = str(mnt)))
+            return redirect(url_for('download_lidar_file', export_filename=export_filename, mnt = str(mnt)))
 
     return render_template('upload_vc_lidar.html')
 
@@ -356,6 +374,11 @@ def upload_vc_lidar():
 @ app.route('/help')
 def about():
     return render_template('help.html')
+
+@app.context_processor
+def inject_app_version():
+    return dict(app_version=APP_VERSION)
+
 
 # Main section ----------------------------------------------------------------
  
